2417db401ebae937a5fdb3f9117a6f59952e03c1
Emma Repp <emmarepp.draws@gmail.com>
2022-01-01 09:27:22 -1000

    commit tree bca5c398c0d2d34cc52df02517647a5eadf0a49e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+   // function superbowlWin(record) {
+      //  
+       // return winningGame.year;
+
+function superbowlWin(record) {
+    //const winningGame = record.find(game => game.result === 'W');
+
+
+    //let winningYear = record.find((record) => record.result === "W");
+    //if (winningGame) {
+      //return winningGame;
+   // } else {
+    //    return undefined
+
+  
+  const superbowlWon = record.find(record => record.result === "W")
+  if (superbowlWon) {
+      return superbowlWon.year;
+  } else {
+      return undefined;
+  }
+}
